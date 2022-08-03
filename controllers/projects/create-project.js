82aee3c5dc3ef919b201a97858eda25edf7a28a4
AnthonyPortier <anthonynicolasportier@gmail.com>
2022-08-03 09:10:11 +0200

    add create project route

---
+++ controllers/projects/create-project.js
@@ -0,0 +1,17 @@
+import pkg from "mongoose";
+const { Types } = pkg;
+import { Project, User } from "../../models";
+
+export const createProject = async (req, res) => {
+  const newProject = new Project({
+    _id: new Types.ObjectId(),
+  });
+  newProject.save();
+
+  await User.findByIdAndUpdate(req.user._id, {
+    $push: {
+      projects: newProject,
+    },
+  });
+  res.json({ project: newProject });
+};
--- routes/projects.js
+++ routes/projects.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
 const projectsRouter = Router();
 import { getProjects } from "../controllers/projects/get-projects";
+import { createProject } from "../controllers/projects/create-project";
+
 
 projectsRouter.get("/", getProjects);
+projectsRouter.post("/create", createProject);
+
 
 export default projectsRouter;

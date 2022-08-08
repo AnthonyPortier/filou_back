fb58fef181dc45e57808da9f42d37170ba16d2ff
AnthonyPortier <anthonynicolasportier@gmail.com>
2022-08-08 11:44:52 +0200

    add 3 new Payslip

--- controllers/projects/create-project.js
+++ controllers/projects/create-project.js
@@ -1,15 +1,18 @@
 import pkg from "mongoose";
 const { Types } = pkg;
-import { Project, User } from "../../models";
+import { Project, User, Payslip } from "../../models";
 
 export const createProject = async (req, res) => {
+  const payslip1 = new Payslip({ _id: new Types.ObjectId() });
+  payslip1.save();
+  const payslip2 = new Payslip({ _id: new Types.ObjectId() });
+  payslip2.save();
+  const payslip3 = new Payslip({ _id: new Types.ObjectId() });
+  payslip3.save();
+
   const newProject = new Project({
     _id: new Types.ObjectId(),
-    payslips: [
-      { _id: new Types.ObjectId() },
-      { _id: new Types.ObjectId() },
-      { _id: new Types.ObjectId() },
-    ],
+    payslips: [payslip1, payslip2, payslip3],
   });
   newProject.save();
 
